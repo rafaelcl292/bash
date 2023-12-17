875d8328e1f962a824fa4fae18f46d6656ee28d0
rafaelcl292 <rafaelcl@al.insper.edu.br>
2023-12-17 00:45:40 -0300

    init

---
+++ binds.sh
@@ -0,0 +1,21 @@
+# Ctrl + F
+fzf_session() {
+    dir=$(fd -t d . ~ ~/.config | fzf)
+    [ -z "$dir" ] && return
+
+    session=$(basename $dir)
+
+    # create a new session if it doesn't exist
+    if ! tmux has-session -t $session 2>/dev/null; then
+        tmux new-session -d -s $session -c $dir
+    fi
+
+    # if in tmux
+    if ! tmux info &> /dev/null; then
+        tmux attach-session -t $session
+    else
+        tmux switch-client -t $session
+    fi
+}
+
+bind -x '"\C-f": fzf_session'
